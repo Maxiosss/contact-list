5d4a9a0b3367187a6851950d38c216c4c8328336
Maxiosss <129985137+Maxiosss@users.noreply.github.com>
2025-12-08 17:34:04 +0100

    persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import contactsReducer from "../features/contacts/contactsSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  contactsState: contactsReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["contactsState"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    contactsState: contactsReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
